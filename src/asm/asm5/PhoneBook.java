1a6c8f0829e02401d3108e1343b531b1d7fecdd6
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-06-02 11:00:54 +0700

    demo asm 5

--- src/asm/asm5/PhoneBook.java
+++ src/asm/asm5/PhoneBook.java
@@ -1,6 +1,7 @@
 package asm.asm5;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class PhoneBook extends Phone{
     public ArrayList<PhoneNumber> phoneList = new ArrayList<>();
@@ -20,21 +21,51 @@ public class PhoneBook extends Phone{
 
     @Override
     public void removePhone(String name) {
-
+        for (PhoneNumber p: phoneList){
+            if(p.getName() == name){
+                phoneList.remove(p);
+                return;
+            }
+        }
     }
 
     @Override
     public void updatePhone(String name, String newphone) {
+        removePhone(name);
+        insertPhone(name,newphone);
 
+//        for (PhoneNumber p: phoneList){
+//            if(p.getName() == name){
+//                p.getPhone().clear();
+//                p.getPhone().add(newphone);
+//                return;
+//            }
+//        }
     }
 
     @Override
     public void searchPhone(String name) {
-
+        for (PhoneNumber p: phoneList){
+            if(p.getName() == name){
+                System.out.println(p.getName());
+                for (String s: p.getPhone()){
+                    System.out.println(s);
+                }
+                return;
+            }
+        }
     }
 
     @Override
     public void sort() {
-
+        phoneList.sort((c1,c2)-> c1.getName().compareTo(c2.getName()));
+//        Comparator<PhoneNumber> cmp = new Comparator<PhoneNumber>() {
+//            @Override
+//            public int compare(PhoneNumber o1, PhoneNumber o2) {
+//                return o1.getName().compareTo(o2.getName());
+//            }
+//        };
+//        phoneList.sort(cmp);
     }
+
 }
